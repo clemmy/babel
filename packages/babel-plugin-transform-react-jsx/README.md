ebe225d49e0dbbdce997c80271a64ab412b24fb0
Clement Hoang <clemmy@fb.com>
2017-09-20 13:46:23 -0700

    Add support for <></> syntax in JSX transform plugin
    Update docs with isFragment
    
    Handle transforms for fragment JSX nodes
    
    Update README for JSX fragment transform
    
    Update reference to state.callee -> state.jsxIdentifier
    
    Revert back to pragma + pragmaFrag instead of pragmaJsx + pragmaJsxFrag naming
    
    Warn if pragma set but pragmafrag not set, and fragment syntax is used
    
    Add tests for JSX fragment syntax
    
    Don't reuse same Node for React.createElement
    
    Put feature flag in front of new compilation features

--- packages/babel-plugin-transform-react-jsx/README.md
+++ packages/babel-plugin-transform-react-jsx/README.md
@@ -9,16 +9,16 @@
 **In**
 
 ```javascript
-var profile = <div>
+var profileFrag = <>
   <img src="avatar.png" className="profile" />
   <h3>{[user.firstName, user.lastName].join(' ')}</h3>
-</div>;
+</>;
 ```
 
 **Out**
 
 ```javascript
-var profile = React.createElement("div", null,
+var profileFrag = React.createElement(React.Fragment, null,
   React.createElement("img", { src: "avatar.png", className: "profile" }),
   React.createElement("h3", null, [user.firstName, user.lastName].join(" "))
 );
@@ -33,20 +33,22 @@ var profile = React.createElement("div", null,
 
 var { dom } = require("deku");
 
-var profile = <div>
+var profileFrag = <>
   <img src="avatar.png" className="profile" />
   <h3>{[user.firstName, user.lastName].join(' ')}</h3>
-</div>;
+</>;
 ```
 
 **Out**
 
 ```javascript
 /** @jsx dom */
+/** @jsxFrag DomFrag */
 
 var dom = require("deku").dom;
+var DomFrag = require("deku").DomFrag; // fictional!
 
-var profile = dom( "div", null,
+var profileFrag = dom(DomFrag, null,
   dom("img", { src: "avatar.png", className: "profile" }),
   dom("h3", null, [user.firstName, user.lastName].join(" "))
 );
@@ -78,7 +80,8 @@ With options:
 {
   "plugins": [
     ["transform-react-jsx", {
-      "pragma": "dom" // default pragma is React.createElement
+      "pragma": "dom", // default is React.createElement
+      "pragmaFrag": "DomFrag", // default is React.Fragment
     }]
   ]
 }
@@ -107,6 +110,12 @@ require("babel-core").transform("code", {
 Replace the function used when compiling JSX expressions.
 
 Note that the `@jsx React.DOM` pragma has been deprecated as of React v0.12
+
+### `pragmaFrag`
+
+`string`, defaults to `React.Fragment`.
+
+Replace the component used when compiling JSX fragments.
 
 ### `useBuiltIns`
 
--- packages/babel-plugin-transform-react-jsx/src/index.js
+++ packages/babel-plugin-transform-react-jsx/src/index.js
@@ -1,8 +1,23 @@
 import jsx from "babel-plugin-syntax-jsx";
 import helper from "babel-helper-builder-react-jsx";
 
+const FEATURE_FLAG_JSX_FRAGMENT = true;
+
+function parseIdentifier(id: string, t) {
+  return id.split(".")
+    .map((name) => t.identifier(name))
+    .reduce((object, property) => t.memberExpression(object, property));
+}
+
+function parseJsxIdentifier(id: string, t) {
+  return id.split(".")
+    .map((name) => t.JSXIdentifier(name))
+    .reduce((object, property) => t.JSXMemberExpression(object, property));
+}
+
 export default function ({ types: t }) {
   const JSX_ANNOTATION_REGEX = /\*?\s*@jsx\s+([^\s]+)/;
+  const JSX_FRAG_ANNOTATION_REGEX = /\*?\s*@jsxFrag\s+([^\s]+)/;
 
   const visitor = helper({
     pre(state) {
@@ -16,34 +31,75 @@ export default function ({ types: t }) {
     },
 
     post(state, pass) {
-      state.callee = pass.get("jsxIdentifier")();
+      state.jsxIdentifier = pass.get("jsxIdentifier")();
     }
   });
 
-  visitor.Program = function (path, state) {
-    const { file } = state;
-    let id = state.opts.pragma || "React.createElement";
+  if (FEATURE_FLAG_JSX_FRAGMENT) {
+    visitor.Program = {
+      enter(path, state) {
+        const { file } = state;
 
-    for (const comment of (file.ast.comments: Array<Object>)) {
-      const matches = JSX_ANNOTATION_REGEX.exec(comment.value);
-      if (matches) {
-        id = matches[1];
-        if (id === "React.DOM") {
-          throw file.buildCodeFrameError(comment,
-            "The @jsx React.DOM pragma has been deprecated as of React 0.12");
-        } else {
-          break;
+        let pragma = state.opts.pragma || "React.createElement";
+        let pragmaFrag = state.opts.pragmaFrag || "React.Fragment";
+        let pragmaSet = !!state.opts.pragma;
+        let pragmaFragSet = !!state.opts.pragmaFrag;
+
+        for (const comment of (file.ast.comments: Array<Object>)) {
+          const jsxMatches = JSX_ANNOTATION_REGEX.exec(comment.value);
+          if (jsxMatches) {
+            pragma = jsxMatches[1];
+            pragmaSet = true;
+            if (pragma === "React.DOM") {
+              throw file.buildCodeFrameError(comment,
+                "The @jsx React.DOM pragma has been deprecated as of React 0.12");
+            }
+          }
+          const jsxFragMatches = JSX_FRAG_ANNOTATION_REGEX.exec(comment.value);
+          if (jsxFragMatches) {
+            pragmaFrag = jsxFragMatches[1];
+            pragmaFragSet = true;
+          }
+        }
+
+        state.set("jsxIdentifier", () => parseIdentifier(pragma, t));
+        state.set("jsxFragIdentifier", () => parseJsxIdentifier(pragmaFrag, t));
+        state.set("usedFragment", false);
+        state.set("pragmaSet", pragmaSet);
+        state.set("pragmaFragSet", pragmaFragSet);
+      },
+      exit(path, state) {
+        const pragmaSet = state.get("pragmaSet");
+        const pragmaFragSet = state.get("pragmaFragSet");
+        const usedFragment = state.get("usedFragment");
+
+        if (pragmaSet && usedFragment && !pragmaFragSet) {
+          throw new Error("transform-react-jsx: pragma has been set but " +
+            "pragmafrag has not been set");
         }
       }
-    }
+    };
+  } else {
+    visitor.Program = function (path, state) {
+      const { file } = state;
+      let id = state.opts.pragma || "React.createElement";
 
-    state.set(
-      "jsxIdentifier",
-      () => id.split(".").map((name) => t.identifier(name)).reduce(
-        (object, property) => t.memberExpression(object, property)
-      )
-    );
-  };
+      for (const comment of (file.ast.comments: Array<Object>)) {
+        const matches = JSX_ANNOTATION_REGEX.exec(comment.value);
+        if (matches) {
+          id = matches[1];
+          if (id === "React.DOM") {
+            throw file.buildCodeFrameError(comment,
+              "The @jsx React.DOM pragma has been deprecated as of React 0.12");
+          } else {
+            break;
+          }
+        }
+      }
+
+      state.set("jsxIdentifier", () => parseIdentifier(id, t));
+    };
+  }
 
   return {
     inherits: jsx,
